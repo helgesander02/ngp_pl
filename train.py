3695123fea9409a64f3dd255e8836da785b1b65d
helgesander02 <98319298+helgesander02@users.noreply.github.com>
2022-11-24 17:30:30 +0800

    Update train.py

--- train.py
+++ train.py
@@ -287,6 +287,9 @@ if __name__ == '__main__':
         imageio.mimsave(os.path.join(system.val_dir, 'rgb.mp4'),
                         [imageio.imread(img) for img in imgs[::2]],
                         fps=30, macro_block_size=1)
+        mageio.mimsave(os.path.join(system.val_dir, 'rgb.gif'),
+                        [imageio.imread(img) for img in imgs[::2]],
+                        'GIF', duration = 0.1)
         imageio.mimsave(os.path.join(system.val_dir, 'depth.mp4'),
                         [imageio.imread(img) for img in imgs[1::2]],
                         fps=30, macro_block_size=1)
